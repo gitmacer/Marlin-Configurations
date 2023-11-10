2ec2c882e0f54e2419a92f2ce2a0de56e5e599b3
Tim Oberle <timfirefox@gmail.com>
2023-11-10 16:45:48 +0100

    more settings

--- config/examples/AnyCubic/i3 Mega/Trigorilla AVR/Configuration.h
+++ config/examples/AnyCubic/i3 Mega/Trigorilla AVR/Configuration.h
@@ -1202,7 +1202,7 @@
  * Override with M92
  *                                      X, Y, Z [, I [, J [, K...]]], E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 96.2 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 408.37 }
 
 /**
  * Default Max Feed Rate (linear=mm/s, rotational=°/s)
@@ -1926,7 +1926,7 @@
  * these options to restore the prior leveling state or to always enable
  * leveling immediately after G28.
  */
-//#define RESTORE_LEVELING_AFTER_G28
+#define RESTORE_LEVELING_AFTER_G28
 //#define ENABLE_LEVELING_AFTER_G28
 
 /**
